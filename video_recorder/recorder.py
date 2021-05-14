b93a3a0badbd5967b64a4b3793c7a8a2225edf26
mkulik05 <mishakulik2@gmail.com>
2021-05-14 16:15:16 +0300

    recording fixes

--- video_recorder/recorder.py
+++ video_recorder/recorder.py
@@ -1,16 +1,22 @@
 from picamera import PiCamera
 from time import sleep
-
 camera = PiCamera()
 import datetime
 import sys
 path = sys.argv[1]
 start_date = datetime.datetime.now()
-interval = 5
+interval = 60
 fps = 10
 while True:
     print(start_date.strftime('%m-%d %H:%M:%S'))
     camera.start_recording('{}/videos/{}.h264'.format(path, start_date.strftime('%m-%d %H:%M:%S')))
-    sleep(interval)
+    camera.wait_recording(interval)
     start_date = datetime.datetime.now()
     camera.stop_recording()
+
+
+
+
+
+
+
